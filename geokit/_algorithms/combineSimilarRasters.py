e45807a2b55eecc52e9537e775e7d487acce05c3
Severin <s.ryberg@fz-juelich.de>
2017-05-25 11:16:48 +0200

    combineSimilarRasters is now working

--- geokit/_algorithms/combineSimilarRasters.py
+++ geokit/_algorithms/combineSimilarRasters.py
@@ -1,116 +1,107 @@
 from geokit._core.regionmask import *
+from os.path import basename
 
 
-def combineSimilarRasters(master, datasets, flipY=False, **kwargs):
+def combineSimilarRasters(master, datasets, combiningFunc=None, verbose=True, **kwargs):
     """!!! WRITE ME !!!"""
-    
-    # Create empty raster containers
-    bands = []
-    extents = []
-    nodataVals = []
 
     # Ensure we have a list of raster datasets
-    if( isinstance(datasets, str) or isinstance(datasets, gdal.Dataset)):
+    if isinstance(datasets, str):
+        datasets = glob(datasets)
+        datasets.sort()
+    elif isinstance(datasets, gdal.Dataset):
         datasets = [datasets,]
-    else:
+    else: # assume datasets is iterable
         datasets = list(datasets)
+
+    if len(datasets)==0: raise GeoKitError("No datasets given")
     
-    # Open all datasets
-    for i in range(len(datasets)):
-        datasets[i] = loadRaster(datasets[i])
+    # Determine info for all datasets
+    infoSet = [rasterInfo(d) for d in datasets]
+
+    # Ensure all input rasters share resolution, srs, datatype, and noData
+    for info in infoSet[1:]:
+        if not info.srs.IsSame(infoSet[0].srs):
+            raise GeoKitError("SRS does not match in datasets")
+        if not (info.dx == infoSet[0].dx and info.dy == infoSet[0].dy) :
+            raise GeoKitError("Resolution does not match in datasets")
+        if not (info.dtype == infoSet[0].dtype) :
+            raise GeoKitError("Datatype does not match in datasets")
     
-    # Open master dataset
-    if(os.path.isfile(master)):
-        masterDS = gdal.Open(master, gdal.GA_Update)
+    # Get summary info about the whole dataset group 
+    dataXMin = min([i.xMin for i in infoSet])
+    dataXMax = max([i.xMax for i in infoSet])
+    dataYMin = min([i.yMin for i in infoSet])
+    dataYMax = max([i.yMax for i in infoSet])
+    
+    # Maybe create a new master dataset
+    if not (os.path.isfile(master)): # we will need to create a master source
+        # Determine no data value
+        noDataValue = kwargs.pop("noDataValue", None)
 
-        # Ensure each datasets has the same projection and resolution as master
-        ras = describeRaster(masterDS)
+        if noDataValue is None:
+            noDataSet = set([i.noData for i in infoSet])
+            if len(noDataSet)==1: noDataValue = noDataSet.pop()
+            
+        # Create Raster
+        dx = infoSet[0].dx
+        dy = infoSet[0].dy
+        dtype = infoSet[0].dtype
+        srs = infoSet[0].srs
+        
+        createRaster(bounds=(dataXMin, dataYMin, dataXMax, dataYMax), output=master, 
+                     dtype=dtype, pixelWidth=dx, pixelHeight=dy, noDataValue=noDataValue, 
+                     srs=srs, fillValue=noDataValue, **kwargs)
 
-        dx = ras.pixelWidth
-        dy = ras.pixelHeight
-        srs = ras.srs
-        xMin, yMin, xMax, yMax = ras.bounds
-        dataType = ras.dtype
+    # Open master dataset and check parameters
+    masterDS = gdal.Open(master, gdal.GA_Update)
+    mInfo = rasterInfo(masterDS)
+    mExtent = Extent(mInfo.bounds, srs=mInfo.srs)
 
-        # Iterate over files
-        for fi in range(len(datasets)):
-            ras = describeRaster(datasets[fi])
-
-            if( dx!=ras.dx or dy!=ras.dy or not srs.IsSame(ras.srs) or dataType!=ras.dtype):
-                msg = "Dataset {0} does not match master configuration".format(fi)
-                raise RuntimeError(msg)
-
-            # Append containers
-            extents.append(ras.bounds)
-            bands.append(datasets[fi].GetRasterBand(1))
-            nodataVals.append( bands[fi].GetNoDataValue() )
-
-    else: # We will need to make a master dataset
-        # Gather collective stats
-        first=True
-        for ds in datasets:
-            ras = describeRaster(ds)
-
-            # Ensure each datasets has the same projection and resolution
-            if(first):
-                first=False
-                dx = ras.pixelWidth
-                dy = ras.pixelHeight
-                srs = ras.srs
-                xMin, yMin, xMax, yMax = ras.bounds
-                dataType = ras.dtype
-            else:
-                if( dx!=ras.dx or dy!=ras.dy or not srs.IsSame(ras.srs) or dataType!=ras.dtype):
-                    raise RuntimeError("Dataset parameters do not match")
-
-            # Update master extent
-            xMin = min((xMin,ras.xMin))
-            yMin = min((yMin,ras.yMin))
-            xMax = max((xMax,ras.xMax))
-            yMax = max((yMax,ras.yMax))
-
-            # Append containers
-            extents.append(ras.bounds)
-            bands.append(ds.GetRasterBand(1))
-            nodataVals.append( bands[-1].GetNoDataValue() )
-
-        # create master dataset
-        createRaster(bounds=(xMin, yMin, xMax, yMax), output=master, dtype=dataType, 
-                            pixelWidth=dx, pixelHeight=dy, srs=srs, **kwargs)
-        masterDS = gdal.Open(master, gdal.GA_Update)
-
+    if not mInfo.srs.IsSame(infoSet[0].srs):
+        raise GeoKitError("SRS's do not match master dataset")
+    if not (mInfo.dx == infoSet[0].dx and mInfo.dy == infoSet[0].dy) :
+        raise GeoKitError("Resolution's do not match master dataset")
+    if not (mInfo.dtype == infoSet[0].dtype) :
+        raise GeoKitError("Datatype's do not match master dataset")
+    
     masterBand = masterDS.GetRasterBand(1)
 
     # Add each dataset to master
     for i in range(len(datasets)):
-        # Calculate starting indicies
-        if(flipY):
-            yStart = int((extents[i][1]-yMin)/dy)
-        else:
-            yStart = int((yMax-extents[i][3])/dy)
-        xStart = int((extents[i][0]-xMin)/dx)
+        if verbose: 
+            if isinstance(datasets[i], str): print(i, basename(datasets[i]))
+            else: print(i)
+        # create dataset extent
+        dExtent = Extent(infoSet[i].bounds, srs=infoSet[i].srs)
 
-        # pull data
-        data = bands[i].ReadAsArray()
-        if (data is None):
-            warnings.warn("Empty raster file? {0}".format(os.path.basename(datasets[i])))
-            continue
-        
+        # Fetch the dataset's matrix
+        dMatrix = fetchMatrix(datasets[i])
+        if not infoSet[i].yAtTop:
+            dMatrix = dMatrix[::-1,:]
+
+        # Calculate starting indicies
+        idx = mExtent.findWithin(dExtent, (mInfo.dx, mInfo.dy), yAtTop=mInfo.yAtTop)
+
         # Get master data
-        mas = masterBand.ReadAsArray(xStart, yStart, data.shape[1], data.shape[0])
+
+        mMatrix = masterBand.ReadAsArray(xoff=idx.xStart, yoff=idx.yStart, win_xsize=idx.xWin, win_ysize=idx.yWin)
 
         # create selector
-        if (not nodataVals[i] is None):
-            sel = np.where( data!=nodataVals[i] )
+        if not combiningFunc is None:
+            writeMatrix = combiningFunc(mMatrix=mMatrix, mInfo=mInfo, dMatrix=dMatrix, dInfo=infoSet[i])
+        elif (not infoSet[i].noData is None):
+            sel = dMatrix!=infoSet[i].noData
+            mMatrix[sel] = dMatrix[sel]
+            writeMatrix = mMatrix
         else: 
-            sel = np.where( data!=0 )
+            writeMatrix = dMatrix
 
         # Add to master
-        mas[sel] = data[sel]
-        masterBand.WriteArray(mas, xStart, yStart)
-
+        masterBand.WriteArray(writeMatrix, idx.xStart, idx.yStart)
+        masterBand.FlushCache()
+    
     # Write final raster
-    masterBand.FlushCache()
     del masterBand
     masterDS.FlushCache()
     calculateStats(masterDS)
