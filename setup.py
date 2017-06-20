44e65882777cd058dc2c021f42cb8750859078f2
Severin Ryberg <SevBerg129@Gmail.com>
2017-06-20 20:54:08 +0200

    VERSION 1.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='geokit',
-    version='0.0.1',
+    version='1.0.0',
     author='Severin Ryberg',
     url='http://www.fz-juelich.de/iek/iek-3/EN/Home/home_node.html',
     packages = ["geokit"],
     install_requires = [
-        "gdal>=2.1.0",
+        "gdal>=2.0.0",
         "numpy>=1.11.2",
     ]
 )
