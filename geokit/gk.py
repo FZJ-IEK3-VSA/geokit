a92c33139ca5411be033888490f1767b8e08664a
Severin Ryberg <SevBerg129@Gmail.com>
2018-05-16 16:15:31 +0200

    Changed convenience import option

---
+++ geokit/gk.py
@@ -0,0 +1,11 @@
+"""
+A convenience module which automatically imports all of geokit modules into the same namespace
+"""
+
+from geokit.util import *
+from geokit.srs  import *
+from geokit.geom  import *
+from geokit.raster  import *
+from geokit.vector  import *
+from geokit import Location, LocationSet, Extent, RegionMask
+from geokit.algorithms import *
