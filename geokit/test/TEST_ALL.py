98253ad2a310ee5cde4bb2a9e4f156ed76340fe7
Severin Ryberg <sevberg129@gmail.com>
2018-05-22 22:16:40 +0200

    Added TEST_ALL macro

---
+++ geokit/test/TEST_ALL.py
@@ -0,0 +1,26 @@
+import os
+
+print("TESTING: util...")
+os.system("python test_util.py")
+
+print("TESTING: srs...")
+os.system("python test_srs.py")
+
+print("TESTING: geom...")
+os.system("python test_geom.py")
+
+print("TESTING: raster...")
+os.system("python test_raster.py")
+
+print("TESTING: vector...")
+os.system("python test_vector.py")
+
+print("TESTING: extent...")
+os.system("python test_extent.py")
+
+print("TESTING: regionmask...")
+os.system("python test_regionmask.py")
+
+print("TESTING: algorithms...")
+os.system("python test_algorithms.py")
+
