a70db5bd6c10fd3515122606dbe4c61f3c2ba384
Severin Ryberg <sevberg129@gmail.com>
2018-05-27 03:01:57 +0200

    fixed bad requirement

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires = [
         "gdal>=2.0.0",
         "numpy>=1.13.2",
-        "descartes>1.1.0",
+        "descartes>=1.1.0",
         "pandas>=0.22.0",
         "scipy>=1.0.0",
         "matplotlib>=2.1.1",
