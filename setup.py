4b057054124766f498fce641ee104407951ce089
Sev <you@example.com>
2019-09-12 13:37:09 +0200

    updated requirements and incremented subversion

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='geokit',
-    version='1.1.2',
+    version='1.1.3',
     author='Severin Ryberg',
     url='https://github.com/FZJ-IEK3-VSA/geokit',
     packages = find_packages(),
     include_package_data=True,
     install_requires = [
-        "gdal>=2.0.0",
+        "gdal==2.4.2",
         "numpy",
         "descartes",
         "pandas",
