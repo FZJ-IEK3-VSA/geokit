bfda5cf484a2f2dcbd2ff781d8421604893e198a
Severin <s.ryberg@fz-juelich.de>
2020-04-08 16:50:36 +0200

    updated version to 1.2.1

--- geokit/__init__.py
+++ geokit/__init__.py
@@ -1,6 +1,6 @@
 """The GeoKit library is a collection of general geospatial operations"""
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 
 # maybe set GDAL_DATA variable
 from os import environ as _environ
@@ -10,17 +10,18 @@ from os.path import join as _join, dirname as _dirname, basename as _basename
 if not "GDAL_DATA" in _environ:
     from os.path import isdir as _isdir
     from sys import executable as _executable
-    
-    for d in [_join(_dirname(_executable),"Library","share","gdal"), # Common location on windows
-              _join(_dirname(_executable),"..","share","gdal")]: # Common location on linux
+
+    for d in [_join(_dirname(_executable), "Library", "share", "gdal"),  # Common location on windows
+              _join(_dirname(_executable), "..", "share", "gdal")]:  # Common location on linux
         if _isdir(d):
             # _warn("Setting GDAL_DATA to: "+d, UserWarning)
             _environ["GDAL_DATA"] = d
             break
 
     if not "GDAL_DATA" in _environ:
-        raise RuntimeError("Could not locate GDAL_DATA folder. Please set this as an environment variable pointing to the GDAL static files")
-    
+        raise RuntimeError(
+            "Could not locate GDAL_DATA folder. Please set this as an environment variable pointing to the GDAL static files")
+
 
 # import the utilities
 import geokit.util
@@ -34,7 +35,7 @@ from geokit.core.location import Location, LocationSet
 from geokit.core.extent import Extent
 from geokit.core.regionmask import RegionMask
 
-# import vidualizing functions to top level since they are 
+# import vidualizing functions to top level since they are
 from geokit.core.util import drawImage
 from geokit.core.geom import drawGeoms
 from geokit.core.raster import drawRaster
