4464ea2bb5e6b579b858123606f9a8e20bdec0e2
Shitab Ishmam <s.ishmam@fz-juelich.de>
2021-04-29 10:35:58 +0200

    added sieve import

--- geokit/raster.py
+++ geokit/raster.py
@@ -17,4 +17,5 @@ from geokit.core.raster import (GeoKitRasterError,
                                 drawSmopyMap,
                                 drawRaster,
                                 warp,
+                                sieve
                                 )
