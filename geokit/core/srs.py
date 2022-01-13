3800eddd0223b6d1e16b5afefd066a9722f7e2c8
c.winkler <c.winkler@fz-juelich.de>
2022-01-13 16:05:49 +0100

    #81 srs validity assert also for loadSRS

--- geokit/core/srs.py
+++ geokit/core/srs.py
@@ -107,6 +107,9 @@ def loadSRS(source):
     if gdal.__version__ >= '3.0.0':
         srs.SetAxisMappingStrategy(osr.OAMS_TRADITIONAL_GIS_ORDER)
 
+    # assert that the srs is valid (may be invalid if e.g. wrong integer codes were passed)
+    assert srs.Validate()==0, f"Created srs is invalid."
+
     return srs
 
 
