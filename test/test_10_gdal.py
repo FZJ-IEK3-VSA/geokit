c0ff8da0cc50b15085cde4ca4cdfef547b5157a3
j.belina <j.belina@fz-juelich.de>
2025-08-19 21:12:08 +0200

    Run contour test on different gdal versions

--- test/test_10_gdal.py
+++ test/test_10_gdal.py
@@ -1,13 +1,17 @@
-# import os
+import os
 
-# import numpy as np
-# import pandas as pd
-# from osgeo import gdal, osr
-# from osgeo.gdal import Driver
-# from osgeo.ogr import Feature, FieldDefn, Layer, OFTInteger, wkbPolygon
+import numpy as np
+import pandas as pd
+from osgeo import gdal, osr
+from osgeo.gdal import Driver
+from osgeo.ogr import Feature, FieldDefn, Layer, OFTInteger, wkbPolygon
 
-# from test.helpers import AACHEN_ELIGIBILITY_RASTER, CLC_RASTER_PATH, result
-
+from test.helpers import (
+    AACHEN_ELIGIBILITY_RASTER,
+    AACHEN_URBAN_LC,
+    CLC_RASTER_PATH,
+    result,
+)
 
 # def test_gdal_warp_basic():
 #     output_path = result("warp_basic.tif")
@@ -130,6 +134,100 @@
 #     assert np.isclose(countour_data_frame.ID[59], 1)
 
 
-# if __name__ == "__main__":
+def test_Extent_contoursFromRaster():
+    ###
+    contourEdges = [1, 2, 3]
+    # Open raster file
+    ds = gdal.Open(AACHEN_URBAN_LC, 0)
+    assert isinstance(ds, gdal.Dataset)
+
+    pass
+    band = ds.GetRasterBand(1)
+    myarray1 = np.array(band.ReadAsArray())
+    unique_arary_values = np.unique(myarray1)
+    pass
+    # gk.drawRaster(band)
+    pass
+    # Set spatial reference system
+    rasterSRS = osr.SpatialReference()
+    _val = rasterSRS.SetFromUserInput(ds.GetProjectionRef())
+    assert _val == 0
+
+    # Create layer
+    driver: Driver = gdal.GetDriverByName("Memory")
+    source: gdal.Dataset = driver.Create("", 0, 0, 0, gdal.GDT_Unknown)
+    layer: Layer = source.CreateLayer("", rasterSRS, wkbPolygon)
+    field = FieldDefn("DN", OFTInteger)
+    layer.CreateField(field)
+
+    # Setup contour function
+    args = []
+    args.append("ID_FIELD=DN")
+    args.append("POLYGONIZE=YES")
+
+    opt = "FIXED_LEVELS="
+    for edge in contourEdges:
+        opt += str(edge) + ","
+    args.append(opt[:-1])
+
+    # Determine contours
+    result = gdal.ContourGenerateEx(band, layer, options=args)
+    layer.CommitTransaction()
+
+    IDs = []
+    geoms = []
+    for ftrid in range(layer.GetFeatureCount()):
+        ftr: Feature = layer.GetFeature(ftrid)
+        geom = ftr.GetGeometryRef()
+        value = ftr.GetField(0)
+        for gi in range(geom.GetGeometryCount()):
+            geoms.append(geom.GetGeometryRef(gi).Clone())
+            IDs.append(value)
+
+    countour_data_frame = pd.DataFrame(dict(geom=geoms, ID=IDs))
+
+    ##
+
+    # assert countour_data_frame.iloc[0].geom.GetSpatialReference().IsSame(ext.srs)
+    try:
+        assert len(countour_data_frame) == 324
+    except AssertionError:
+        raise (
+            AssertionError(
+                " The length of the data frame was "
+                + str(len(countour_data_frame))
+                + " even though 324 was expected"
+            )
+        )
+    try:
+        assert np.isclose(countour_data_frame.iloc[63].geom.Area(), 285000.2699996233)
+    except AssertionError:
+        raise (
+            AssertionError(
+                " The area was"
+                + str(countour_data_frame.iloc[63].geom.Area())
+                + " even though 285000.2699996233 was expected"
+            )
+        )
+    try:
+        assert countour_data_frame.iloc[63].ID == 1
+    except AssertionError:
+        raise (
+            AssertionError(
+                " The index was"
+                + str(countour_data_frame.iloc[63].geom.Area())
+                + " even though 285000.2699996233 was expected"
+            )
+        )
+
+    # assert len(countour_data_frame) == 95
+    # assert np.isclose(
+    #     countour_data_frame.iloc[63].geom.Area(), 0.08834775465377398
+    # )  # index of geom changed from 61 to 63 with GDAL >= 3.0.0
+    # assert countour_data_frame.iloc[63].ID == 1
+
+
+if __name__ == "__main__":
+    test_Extent_contoursFromRaster()
 #     # test_contours()
 #     test_contours_pure_gdal()
