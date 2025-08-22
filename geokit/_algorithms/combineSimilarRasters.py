6530ab948df2f73bf22eb46d33688bae4462624c
Christoph Winkler <c.winkler@fz-juelich.de>
2025-08-22 19:21:39 +0200

    update imports

--- geokit/_algorithms/combineSimilarRasters.py
+++ geokit/_algorithms/combineSimilarRasters.py
@@ -9,7 +9,7 @@ from osgeo import gdal
 
 from geokit.core.regionmask import *
 from geokit.core.util import GeoKitError, get_common_dtype
-from geokit.raster import createRaster, extractMatrix, rasterInfo, warp, loadRaster
+from geokit.raster import createRaster, extractMatrix, rasterInfo, loadRaster, RasterInfo
 
 
 def checkSimilarRasters(
