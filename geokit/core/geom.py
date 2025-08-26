19f9351ff1a26d8b078c9587189ecb69e7d3e96d
Christoph Winkler <c.winkler@fz-juelich.de>
2025-08-26 18:09:38 +0200

    use inbuilt GeoKitGeomError

--- geokit/core/geom.py
+++ geokit/core/geom.py
@@ -1875,9 +1875,8 @@ def applyBuffer(geom, buffer, applyBufferInSRS=False, split="shift"):
         south_dist = transform(_geom, toSRS=south_srs).Distance(
             point(0, 0, srs=south_srs)
         )
-        assert min([north_dist, south_dist]) > buffer_mtrs, (
-            f"buffered geometry would intersect with North or South Pole."
-        )
+        if min([north_dist, south_dist]) <= buffer_mtrs: 
+            raise GeoKitGeomError(f"buffered geometry would intersect with North or South Pole.")
 
         # convert geom to applyBufferInSRS
         _geom = transform(_geom, toSRS=applyBufferInSRS)
