6818b3f4032ab1ab605e008562b60b2751dee29d
j.belina <j.belina@fz-juelich.de>
2025-10-06 21:41:39 +0200

    Fix two typos

--- geokit/core/regionmask.py
+++ geokit/core/regionmask.py
@@ -269,7 +269,6 @@ class RegionMask(object):
         srs : Anything acceptable to geokit.srs.loadSRS()
             The srs context of the generated RegionMask object
             * This srs is superseded by the srs in an explicitly defined extent
-            * The default srs EPSG3035 is only valid for a European context
 
         extent : Extent object
             The geospatial context of the generated region mask
@@ -362,7 +361,6 @@ class RegionMask(object):
         srs : Anything acceptable to geokit.srs.loadSRS()
             The srs context of the generated RegionMask object
             * This srs is superseded by the srs in an explicitly defined extent
-            * The default srs EPSG3035 is only valid for a European context
 
         extent : Extent object
             The geospatial context of the generated region mask
